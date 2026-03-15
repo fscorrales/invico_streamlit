07471dff186daf092bc3d7cc9e9d92f9a891d9e1
Fernando Corrales <fscorrales@gmail.com>
2026-03-15 11:27:21 -0300

    refactor siif's main page

--- src/pages/tablas_auxiliares/siif/siif.py
+++ src/pages/tablas_auxiliares/siif/siif.py
@@ -6,21 +6,6 @@ from src.pages.tablas_auxiliares.siif import siif_rf602, siif_rf610
 
 
 def main() -> None:
-    # # Sidebar menu logic
-    # st.sidebar.markdown("### Menú SIIF")
-
-    # # We use radio buttons as they match the list style closely
-    # menu_selection = st.sidebar.radio(
-    #     "Reportes Disponibles",
-    #     options=["RF602", "RF610"],
-    #     label_visibility="collapsed",
-    # )
-
-    # # Render selected sub-page
-    # if menu_selection == "RF602":
-    #     siif_rf602.render()
-    # elif menu_selection == "RF610":
-    #     siif_rf610.render()
 
     tab1, tab2 = st.tabs(["RF602", "RF610"])
 
