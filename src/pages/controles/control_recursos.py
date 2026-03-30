bbe316fed196ad7314377d71dfe976a6c617d902
Fernando Corrales <fscorrales@gmail.com>
2026-03-30 20:52:13 -0300

    v0.12.30: Update Control Recursos

--- src/pages/controles/control_recursos.py
+++ src/pages/controles/control_recursos.py
@@ -3,7 +3,6 @@ Author: Fernando Corrales <fscpython@gmail.com>
 Purpose: SIIF Recursos vs SSCC depósitos
 Data required:
     - SIIF rci02
-    - SIIF ri102 (no obligatorio)
     - SSCC Consulta General de Movimiento
     - SSCC ctas_ctes (manual data)
 Google Sheet:
@@ -15,9 +14,12 @@ import subprocess
 import sys
 
 import streamlit as st
+from playwright.async_api import async_playwright
 
+from src.automation.siif.rci02 import Rci02
 from src.constants.endpoints import Endpoints
 from src.constants.options import get_ejercicios_list
+from src.services.api_client import post_request
 from src.views.aux_tables import report_template
 from src.views.modals import request_siif_and_sscc_credentials_modal
 
@@ -43,15 +45,39 @@ async def run_automation(
     ejercicios_str = ",".join(map(str, ejercicios))
 
     # 2. Iniciamos la descarga automática
-    results = []
-    # 2.a. Ejecutamos el módulo runner de SSCC en un proceso separado
+    # 2.a. Ejecutamos la automatización de SIIF
+    async with async_playwright() as p:
+        siif = Rci02()
+        # The Rf602 class handles login via SIIFReportManager.login
+        await siif.login(
+            username=siif_username,
+            password=siif_password,
+            playwright=p,
+            headless=False,
+        )
+        await siif.go_to_reports()
+
+        results = []
+        for ej in ejercicios:
+            df_clean = await siif.download_and_process_report(ejercicio=ej)
+            if df_clean is not None and not df_clean.empty:
+                # Send to backend
+                json_data = df_clean.to_dict(orient="records")
+                response = post_request(Endpoints.SIIF_RCI02.value, json_body=json_data)
+                results.append(f"Ejercicio {ej}: {response}")
+
+        await siif.logout()
+
+    print("✅ SIIF Finalizado. Iniciando SSCC...")
+
+    # 2.b. Ejecutamos el módulo runner de SSCC en un proceso separado
     modulo_runner = "src.automation.sscc.banco_invico_runner"
 
     # Aseguramos que el PYTHONPATH sea la raíz actual
     env = os.environ.copy()
     env["PYTHONPATH"] = os.getcwd()
 
-    results = subprocess.Popen(
+    process_sscc = subprocess.Popen(
         [
             sys.executable,
             "-m",
@@ -64,28 +90,10 @@ async def run_automation(
         creationflags=subprocess.CREATE_NEW_CONSOLE,
         env=env,
     )
-    # 2.b. Ejecutamos la automatización de SIIF
-    # async with async_playwright() as p:
-    #     siif = Rf602()
-    #     # The Rf602 class handles login via SIIFReportManager.login
-    #     await siif.login(
-    #         username=username,
-    #         password=password,
-    #         playwright=p,
-    #         headless=False,
-    #     )
-    #     await siif.go_to_reports()
 
-    #     results = []
-    #     for ej in ejercicios:
-    #         df_clean = await siif.download_and_process_report(ejercicio=ej)
-    #         if df_clean is not None and not df_clean.empty:
-    #             # Send to backend
-    #             json_data = df_clean.to_dict(orient="records")
-    #             response = post_request(ENDPONT, json_body=json_data)
-    #             results.append(f"Ejercicio {ej}: {response}")
-
-    #     await siif.logout()
+    # Esperamos que el SSCC termine antes de devolver el control a Streamlit
+    process_sscc.wait()
+    print("✅ SSCC Finalizado.")
 
     return results
 
