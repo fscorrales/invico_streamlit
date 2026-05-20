481fe76408aff462aadf9a5369b4aa6bedee361f
Fernando Corrales <fscpython@gmail.com>
2026-05-20 10:02:18 -0300

    v0.20.0: Enhance button components with additional functionality

---
+++ src/components/__init__.py
@@ -0,0 +1,5 @@
+from .buttons import *
+from .cards import *
+from .dataframes import *
+from .multiselects import *
+from .text_inputs import *
--- src/components/buttons.py
+++ src/components/buttons.py
@@ -1,15 +1,72 @@
+__all__ = [
+    "button_update",
+    "button_export",
+    "button_submit",
+    "button_cancel",
+    "button_add",
+    "button_edit",
+    "button_delete",
+    "button_selfadd",
+]
+
 import streamlit as st
+
+DEFAULT_WIDTH = 120
 
 
 # --------------------------------------------------
 def button_update(label: str, key: str = "button_update", **kwargs) -> bool:
     """Un componente reutilizable que retorna True si se presiona."""
     with st.container(border=False, width="content"):
-        return st.button("🔄 " + label, key=key, **kwargs)
+        return st.button("🔄 " + label, key=key, width=DEFAULT_WIDTH, **kwargs)
 
 
 # --------------------------------------------------
 def button_export(label: str, key: str = "button_export", **kwargs):
     """Un componente reutilizable"""
     with st.container(border=False, width="content"):
-        return st.button("🔄 " + label, key=key, **kwargs)
+        return st.button("📤 " + label, key=key, width=DEFAULT_WIDTH, **kwargs)
+
+
+# --------------------------------------------------
+def button_submit(label: str, key: str = "button_submit", **kwargs):
+    """Un componente reutilizable"""
+    with st.container(border=False, width="content"):
+        return st.button(
+            "🗳️ " + label, key=key, width=DEFAULT_WIDTH, **kwargs, type="primary"
+        )
+
+
+# --------------------------------------------------
+def button_cancel(label: str, key: str = "button_cancel", **kwargs):
+    """Un componente reutilizable"""
+    with st.container(border=False, width="content"):
+        return st.button("❌ " + label, key=key, width=DEFAULT_WIDTH, **kwargs)
+
+
+# --------------------------------------------------
+def button_add(label: str, key: str = "button_add", **kwargs):
+    """Un componente reutilizable"""
+    with st.container(border=False, width="content"):
+        return st.button("💾 " + label, key=key, width=DEFAULT_WIDTH, **kwargs)
+
+
+# --------------------------------------------------
+def button_edit(label: str, key: str = "button_edit", **kwargs):
+    """Un componente reutilizable"""
+    with st.container(border=False, width="content"):
+        return st.button("✏️ " + label, key=key, width=DEFAULT_WIDTH, **kwargs)
+
+
+# --------------------------------------------------
+def button_delete(label: str, key: str = "button_delete", **kwargs):
+    """Un componente reutilizable"""
+    with st.container(border=False, width="content"):
+        return st.button("🗑️ " + label, key=key, width=DEFAULT_WIDTH, **kwargs)
+
+
+# --------------------------------------------------
+def button_selfadd(label: str, key: str = "button_selfadd", **kwargs):
+    """Un componente reutilizable"""
+    with st.container(border=False, width="content"):
+        return st.button("🔮 " + label, key=key, width=DEFAULT_WIDTH, **kwargs)
