06a0cb6bcf03446f4e2ded4c338896df1cf8a5ba
Fernando Corrales <fscpython@gmail.com>
2026-06-02 12:17:34 -0300

    v0.50.0: Add obras functionality to ICARO module

--- src/pages/tablas_auxiliares/icaro/icaro.py
+++ src/pages/tablas_auxiliares/icaro/icaro.py
@@ -2,17 +2,20 @@
 
 import streamlit as st
 
-from src.pages.tablas_auxiliares.icaro import carga, estructuras
+from src.pages.tablas_auxiliares.icaro import carga, estructuras, obras
 
 
 def main() -> None:
-    tab_carga, tab_estructura = st.tabs(["Carga", "Estructura"])
+    tab_carga, tab_estructura, tab_obras = st.tabs(["Carga", "Estructura", "Obras"])
 
     with tab_carga:
         carga.render()
 
     with tab_estructura:
         estructuras.render()
+
+    with tab_obras:
+        obras.render()
 
 
 if __name__ == "__main__":
